d625f666ee396162c8ab87baf039d66b98cd0d35
Marcos Brenes <dev.virco@gmail.com>
2015-05-02 18:25:05 -0400

    new accounts can be created, fetches user data from FB into ParseUser

--- OnMyWay/src/dev/virco/onmyway/MainActivity.java
+++ OnMyWay/src/dev/virco/onmyway/MainActivity.java
@@ -3,22 +3,34 @@ package dev.virco.onmyway;
 
 import java.util.ArrayList;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
+import com.facebook.AccessToken;
+import com.facebook.GraphRequest;
+import com.facebook.GraphRequest.GraphJSONObjectCallback;
+import com.facebook.GraphResponse;
 import com.parse.LogInCallback;
 import com.parse.ParseException;
 import com.parse.ParseFacebookUtils;
 import com.parse.ParseUser;
+import com.parse.SaveCallback;
 
 public class MainActivity extends Activity {
 	
 	Button loginButton;
+	ProgressDialog dialog = null;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -29,21 +41,82 @@ public class MainActivity extends Activity {
 		loginButton.setOnClickListener(new View.OnClickListener() {
 			
 			@Override
-			public void onClick(View v) {
+			public void onClick(final View v) {
 				v.setEnabled(false);
-				ParseFacebookUtils.logInWithReadPermissionsInBackground(getParent(), new ArrayList<String>(), new LogInCallback() {
+				ParseFacebookUtils.logInWithReadPermissionsInBackground(MainActivity.this, new ArrayList<String>(), new LogInCallback() {
 					
 					@Override
 					public void done(ParseUser user, ParseException e) {
-						// TODO Auto-generated method stub
-						
+						dialog = new ProgressDialog(MainActivity.this);
+						if (user == null) {
+							Log.d("omw", "Uh oh. The user cancelled the Facebook login.");
+						    toastMessage("Facebook Login Canceled.");
+						    v.setEnabled(true);
+						} else if (user.isNew()) {
+							dialog.setMessage("Creating new account...");
+							dialog.show();
+							getFacebookIdInBackground();
+						    Log.d("omw", "User signed up and logged in through Facebook!");
+						} else {
+							Log.d("omw", "User logged in through Facebook!");
+						}
 					}
 				});
-				v.setEnabled(true);
 			}
 		});
 	}
+	
 
+	private void getFacebookIdInBackground() {
+		
+	 GraphRequest request = GraphRequest.newMeRequest(AccessToken.getCurrentAccessToken(), new GraphRequest.GraphJSONObjectCallback() {
+		
+		@Override
+		public void onCompleted(JSONObject object, GraphResponse response) {
+			if (object != null) {
+				Log.d("omw", object.toString());
+				String fbID = null;
+				String fullName = null;
+				String firstName = null;
+				try {
+					fbID = object.getString(OMWConstants.Facebook.ID);
+					fullName = object.getString(OMWConstants.Facebook.FULL_NAME);
+					firstName = object.getString(OMWConstants.Facebook.FIRST_NAME);
+				} catch (JSONException e) {
+					Log.e("omw", e.getMessage());
+					if (dialog != null) dialog.dismiss();
+				}
+				
+				ParseUser currentUser = ParseUser.getCurrentUser();
+				currentUser.put(OMWConstants.ParseUser.FACEBOOK_ID, fbID);
+				currentUser.put(OMWConstants.ParseUser.FIST_NAME, firstName);
+				currentUser.put(OMWConstants.ParseUser.FULL_NAME, fullName);
+				currentUser.saveInBackground(new SaveCallback() {
+					
+					@Override
+					public void done(ParseException e) {
+						startListActivity();
+					}
+				});
+			} else {
+				if (dialog != null) dialog.dismiss();
+				toastMessage("Couldn't create account details.");
+			}
+		}
+	 });
+	 
+	 Bundle parameters = new Bundle();
+	 parameters.putString("fields", "id,name,first_name");
+	 request.setParameters(parameters);
+	 request.executeAsync();
+	 
+	}
+	
+	public void startListActivity() {
+		if (dialog != null ) dialog.dismiss();
+		toastMessage("StartListActivity :)");
+	}
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
@@ -63,5 +136,9 @@ public class MainActivity extends Activity {
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 		super.onActivityResult(requestCode, resultCode, data);
 		ParseFacebookUtils.onActivityResult(requestCode, resultCode, data);
+	}
+	
+	public void toastMessage(String message) {
+		Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
 	}
 }
---
+++ OnMyWay/src/dev/virco/onmyway/OMWConstants.java
@@ -0,0 +1,16 @@
+package dev.virco.onmyway;
+
+public class OMWConstants {
+	
+	public static class ParseUser {
+		public static final String FACEBOOK_ID = "fbID";
+		public static final String FIST_NAME = "firstName";
+		public static final String FULL_NAME = "fullName";
+	}
+	
+	public static class Facebook {
+		public static final String ID = "id";
+		public static final String FIRST_NAME = "first_name";
+		public static final String FULL_NAME = "name";
+	}
+}
